b21ad9b45e79ae90bf4419dbe394b983c14e4b8c
Jacob Philpott <jacob.gary.philpott@gmail.com>
2019-05-03 16:22:37 +0300

    setup data processing

--- bitgov/protocol/client.py
+++ bitgov/protocol/client.py
@@ -1,18 +1,17 @@
 from socket import socket
+from multiprocessing import Process
+from bitgov.protocol.utilities import process_incoming, process_outgoing
 
-def client_send(HOST, PORT, IPv, PROTOCOL, BUFF, message):
+def client_broadcast(HOST, PORT, IPv, PROTOCOL, data):
+    try:
+        with socket(IPv, PROTOCOL) as sock:
+            sock.connect((HOST, PORT))
+            print("\n\033[0;33mBroadcasting to: \033[1;33m{}:{}\033[0;0m".format(HOST, PORT))
+            Process(target=client_send, args=(sock, data)).start()
+    except:
+        print("\n\033[0;31mBroadcast Error!\033[0;0m ⛔\n")
 
-    with socket(IPv, PROTOCOL) as sock:
-
-        sock.connect((HOST, PORT))
-        sock.sendall(bytes(str(message), "utf-8"))
-
-        response = b""
-
-        while True:
-            fragment = sock.recv(BUFF)
-            response += fragment
-            if len(fragment) < BUFF:
-                break
-
-        print("Received:", response)
+def client_send(connection, data):
+    connection.sendall(process_outgoing(data))
+    response = process_incoming(connection)
+    print("\033[1;32mResponse:\033[0;32m {}\033[0;0m\n".format(response))
--- bitgov/protocol/server.py
+++ bitgov/protocol/server.py
@@ -1,45 +1,32 @@
 from socket import socket
 from threading import Thread
 from multiprocessing import Process
-from bitgov.utilities import get_nodes
+from bitgov.protocol.utilities import process_incoming, process_outgoing
 
-def server_config(HOST, PORT, IPv, PROTOCOL, BUFF):
-    def server_accept(sock):
-        def server_connection(connection, address):
-
-            connection.setblocking(False)
-
-            with connection:
-                print("Connected with:", address)
-                request = b""
-                while True:
-                    try:
-                        fragment = connection.recv(BUFF)
-                    except:
-                        break
-                    request += fragment
-
-                print("Received:", request)
-                connection.sendall(request)
-
-        while True:
-            connection, address = sock.accept()
-            Thread(target=server_connection, args=(connection, address)).start()
+def server_config(HOST, PORT, IPv, PROTOCOL):
 
     print("\033[0;33mSetting up the server.. \033[0;0m", end="")
 
     try:
         with socket(IPv, PROTOCOL) as sock:
-            try:
-                sock.bind((HOST, PORT))
-                sock.listen()
-                server = Process(target=server_accept, args=(sock,))
-                server.start()
-                print("\033[1;32mSuccess!\033[0;0m 👍")
-                get_nodes(server)
-            except:
-                print("\033[0;31mPort Error!\033[0;0m ⛔")
-                get_nodes(None)
+            sock.bind((HOST, PORT))
+            sock.listen()
+            server = Process(target=server_accept, args=(sock,))
+            server.start()
+            print("\033[1;32mSuccess!\033[0;0m 👍")
+            print("\033[0;33mServer running on port: \033[1;33m{}\033[0;0m\n".format(str(PORT)))
+            return server
     except:
-        print("\033[0;31mConfiguration Error!\033[0;0m ⛔")
-        get_nodes(None)
+        print("\033[0;31mConfiguration Error!\033[0;0m ⛔\n")
+
+def server_accept(sock):
+    while True:
+        connection, address = sock.accept()
+        Thread(target=server_connection, args=(connection, address)).start()
+
+def server_connection(connection, address):
+    with connection:
+        print("\033[0;33mConnected with: \033[1;33m{}:{}\033[0;0m".format(address[0], address[1]))
+        request = process_incoming(connection)
+        print("\033[1;32mReceived:\033[0;32m {}\033[0;0m\n".format(request))
+        connection.sendall(process_outgoing(request))
